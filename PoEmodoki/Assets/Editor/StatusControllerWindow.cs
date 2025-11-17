2d82bf58c89bc2655d2bde62d77d54a0dfd64c5e
TAKERU\taken <takenoko702takeru@gmail.com>
2025-11-17 16:40:39 +0900

    おっぱぶいくかぁ

--- PoEmodoki/Assets/Editor/StatusControllerWindow.cs
+++ PoEmodoki/Assets/Editor/StatusControllerWindow.cs
@@ -5,39 +5,74 @@ public class StatusControllerWindow : EditorWindow
 {
     [MenuItem("Tools/Status Controller")]
 
+    
     public static void ControllerWindow()
     {
         GetWindow<StatusControllerWindow>("Controller Window");
     }
 
+    private IStatusView selectedTarget;
+    private SerializedObject serializeStatus;
+
+    private void OnSelectionChange()
+    {
+        UpDateTarget();
+    }
+    private void OnFocus()
+    {
+        UpDateTarget();
+    }
+
+    private void UpDateTarget()
+    {
+        selectedTarget = null;
+        serializeStatus = null;
+
+        GameObject selectedObj = Selection.activeGameObject;
+
+        if (selectedObj != null)
+        {
+            selectedTarget = selectedObj.GetComponent<IStatusView>();
+            if (selectedTarget != null)
+            {
+                serializeStatus = selectedTarget.GetSerializedBaseStatus();
+            }
+        }
+        Repaint();
+    }
+
     private void OnGUI()
     {
         GUILayout.Label("編集", EditorStyles.boldLabel);
-        if(Application.isPlaying && test.Instance != null)
+        if(selectedTarget == null)
         {
-            int newHp = EditorGUILayout.IntField("敵のHP:", test.Instance.EnemyHp);
-            if (newHp != test.Instance.EnemyHp)
+            EditorGUILayout.LabelField("編集対象を選択してください");
+            return;
+        }
+
+        //---------------------------------------------------------
+        //基礎設計値を編集
+        if(serializeStatus != null)
+        {
+            GUILayout.Label($"基本設計値: {serializeStatus.targetObject.name} (編集可)",EditorStyles.boldLabel);
+
+            serializeStatus.Update();
+
+
+            SerializedProperty iterator = serializeStatus.GetIterator();
+            iterator.NextVisible(true);
+            while (iterator.NextVisible(false))
             {
-                test.Instance.EnemyHp = newHp;
+                EditorGUILayout.PropertyField(iterator, true);
             }
+
+            serializeStatus.ApplyModifiedProperties();
         }
         else
         {
-            EditorGUILayout.LabelField("ゲームスタッツが初期化されてないゾ");
-            EditorGUILayout.LabelField("ゲームが開始されてないゾ");
+            EditorGUILayout.LabelField("編集可能な基本設計値がありません");
         }
-    }
-    void Start()
-    {
         
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Application.isPlaying)
-        {
-            Repaint();
-        }
     }
 }
