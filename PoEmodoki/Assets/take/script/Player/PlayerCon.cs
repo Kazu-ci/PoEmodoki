864c64c9150a04411563f4667212b74f3abbfb8e
TAKERU\taken <takenoko702takeru@gmail.com>
2025-11-25 13:35:57 +0900

    プレイヤーの挙動に取り掛かったよ おっぱい

--- PoEmodoki/Assets/take/script/Player/PlayerCon.cs
+++ PoEmodoki/Assets/take/script/Player/PlayerCon.cs
@@ -1,53 +1,62 @@
 using System;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-[RequireComponent(typeof(Rigidbody))]
-public class PlayerCon : MonoBehaviour
+
+
+public class PlayerCon : MonoBehaviour,IStatusView
 {
+    [SerializeField]PlayerStatus player;
+    [SerializeField]SerializedObject sPlayerStatus;
+    [SerializeField] PlayerInput PlayerInput;
+    public InputAction action;
+    int MaxHP;
+    int Defense;
+    int MP;
+    float MoveSpeed;
+    float AtkSpeed;
+    float CastSpeed;
+    float Length;
+    float ElementDefense;
+    float Critical;
 
-    [Header("プレイヤーの移動設定")]
-    [SerializeField] private float _moveFroce = 5f;
-    [SerializeField] private float _moveSpeed = 5f;
-
-    private Rigidbody _rigidbody;
-    private InputSystem_Actions _inputActions;
-    private Vector2 _moveInputValues;
-
-
+    
+    
     private void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-        //アクションのインスタンス化
-        _inputActions = new InputSystem_Actions();
-        //Actionイベントの登録
-        _inputActions.Player.Move.started += OnMove;
-        _inputActions.Player.Move.performed += OnMove;
-        _inputActions.Player.Move.canceled += OnMove;
-        //随時追加
-        //=============================================
-
-        _inputActions.Enable();
+        MaxHP = player.PlayerHp;
+        Defense = player.PlayerDefense;
+        MP = player.PlayerMp;
+        MoveSpeed = player.PlayerSpeed;
+        AtkSpeed = player.PlayerAtkSpeed;
+        CastSpeed = player.PlayerCastSpeed;
+        Length = player.PlayerLength;
+        ElementDefense = player.PlayerElementDefense;
+        Critical = player.PlayerCritical;
     }
-
-    private void OnDestroy()
-    {
-        //自身でInstance化したActionクラスはIDisposableを実装してるので、Disposeして開放する必要がある
-        _inputActions?.Dispose();
-    }
-
-    private void OnMove(InputAction.CallbackContext context)
-    {
-        //MoveActionの入力取得
-        _moveInputValues = context.ReadValue<Vector2>();
-    }
-
-    // Update is called once per frame
-    private void FixedUpdate()
+    void Update()
     {
         //移動処理（仮）
-        _rigidbody.AddForce(new Vector3(_moveInputValues.x * _moveFroce, 0, _moveInputValues.y * _moveFroce));
+        MovePlayer();
+        
+    }
+
+    void MovePlayer()
+    {
+        var input = PlayerInput.actions["Move"].ReadValue<Vector2>();
+        transform.Translate(new Vector3(input.x,0,input.y)*MoveSpeed);
+    }
+
+    public void DrawRunningStatusGUI()
+    {
+
+    }
+
+    public SerializedObject GetSerializedBaseStatus()
+    {
+        return sPlayerStatus;
     }
 }
