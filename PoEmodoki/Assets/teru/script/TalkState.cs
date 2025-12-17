e169e462af251a293c8baa52a5b6ee3985f080a8
CI <CI@jec.ac.jp>
2025-12-17 16:41:27 +0900

    手間がかかる女だこと

--- PoEmodoki/Assets/teru/script/TalkState.cs
+++ PoEmodoki/Assets/teru/script/TalkState.cs
@@ -1,20 +1,21 @@
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.Android;
 
 public class TalkState : StateMachine<GameCon>.StateBase
 {
     public override void OnStart()
     {
-        Time.timeScale = 0f;
+        Debug.Log("Talk");
+        //Time.timeScale = 0f;
     }
 
     public override void OnUpdate()
     {
         // 会話中の特殊な監視ロジック（例：Fungus完了待ちなど）
 
-        // Fungusが終了したら ChangeState(State.Combat) を呼び出す必要がある。
         // (例: Flowchart.ExecuteBlock("EndTalk") の最後に Call Method で Combat に戻す)
     }
-
     public override void OnEnd()
     {
         // TalkState終了時のクリーンアップ
