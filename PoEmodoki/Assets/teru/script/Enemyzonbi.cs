bf85be2d39d85cfb0ce097ddbd917f2432e26157
reonary <24ci0127@jec.ac.jp>
2026-01-27 10:03:22 +0900

    俺の進捗系飛ばしたやつ殺す

--- PoEmodoki/Assets/teru/script/Enemyzonbi.cs
+++ PoEmodoki/Assets/teru/script/Enemyzonbi.cs
@@ -18,7 +18,8 @@ public class Enemyzonbi : Enemy
     private Dictionary<string, Collider> colliderDict;
     private Dictionary<string, GameObject> effectDict;
     private SkillStatus currentSkill;
-
+    [SerializeField] int dropcount = 1;//ドロップするソウルの数
+    [SerializeField] private GameObject soulprefab;//ドロップさせるソウルの種類
     protected enum State
     {
         Idle,
@@ -274,5 +275,15 @@ public class Enemyzonbi : Enemy
             //Owner.enemyAnimation.ResetTrigger("Combo");
         }
     }
-
+    protected override void Drop()
+    {
+        if (soulprefab == null)
+        {
+            return;
+        }
+        for (int i = 0; i < dropcount; ++i)
+        {
+            Instantiate(soulprefab, thisobj.transform.position, Quaternion.identity);
+        }
+    }
 }
